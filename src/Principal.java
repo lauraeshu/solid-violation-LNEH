02f522de967ae1164ba99f7aba0cb35d45612cf9
lauraeshu <72833286+lauraeshu@users.noreply.github.com>
2022-07-25 18:36:10 -0500

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,34 @@
-public class Principal {
+public class Principal implements Envioi {
+    
 
-  public static void main(String[] args) {
-    Envio[] envios = {
-        new Envio("Municipal",5000),
-        new Envio("Intermunicipal",15000),
-        new Envio("Internacional",25000)
-    };
-    imprimirTiempoDeEnvio(envios);
-    imprimirTiempoDeEnvio(envios);
-  }
+    public static void main(String[] args) {
 
-  public static void imprimirTiempoDeEnvio(Envio[] envios){
-    for (Envio coche : envios) {
-      if(coche.tipo.equals("Municipal")) System.out.println("Tiempo envio 12 horas");
-      if(coche.tipo.equals("Intermunicipal")) System.out.println("Tiempo envio 36 horas");
-      if(coche.tipo.equals("Internacional")) System.out.println("Tiempo envio 90 horas");
+        Envio[] envios = {
+                new Envio("Municipal",5000),
+                new Envio("Intermunicipal",15000 ),
+                new Envio("Internacional",25000)
+        };
+        
+        imprimir();
+        
+        
     }
-  }
+
+    private static void imprimir() {
+    }
+
+    @Override
+    public void imprimir(Envio [] envios) {
+        for (Envio coche : envios) {
+
+            if (coche.tipo.equals("Municipal")) System.out.println("Tiempo envio 12 horas");
+            if (coche.tipo.equals("Intermunicipal")) System.out.println("Tiempo envio 36 horas");
+            if (coche.tipo.equals("Internacional")) System.out.println("Tiempo envio 90 horas");
+
+        }
+    }
+
+    
 }
+
+
